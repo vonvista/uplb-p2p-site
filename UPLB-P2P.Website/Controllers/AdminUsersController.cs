8891929ee448a03eb74fe9c38123b4d7b7884eaa
Iggy <lbvillapando@up.edu.ph>
2023-09-28 23:38:38 +0800

    +added basic add user feature

---
+++ UPLB-P2P.Website/Controllers/AdminUsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UPLB_P2P.Website.Data;
+using UPLB_P2P.Website.Models.Domain;
+using UPLB_P2P.Website.Models.ViewModels;
+
+namespace UPLB_P2P.Website.Controllers
+{
+    public class AdminUsersController : Controller
+    {
+        private P2PDbContext _p2pDbContext;
+        public AdminUsersController(P2PDbContext p2pDbContext)
+        {
+            _p2pDbContext = p2pDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ActionName("Add")]
+        public IActionResult Add(AddUserRequest addUserRequest)
+        {
+            //var username = Request.Form["username"];
+            //var email = Request.Form["email"];
+            //var password = Request.Form["password"];
+
+            //var username = addUserRequest.Username;
+            //var email = addUserRequest.Email;
+            //var password = addUserRequest.Password;
+            //DateTime createDate = DateTime.Now;
+
+            //mapping the AddUserRequest request to the User domain model
+            var newUser = new User
+            {
+                Username = addUserRequest.Username,
+                Email = addUserRequest.Email,   
+                Password = addUserRequest.Password,
+                CreatedAt = DateTime.Now
+            };
+
+            _p2pDbContext.Users.Add(newUser);
+            _p2pDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ UPLB-P2P.Website/Models/ViewModels/AddUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace UPLB_P2P.Website.Models.ViewModels
+{
+    public class AddUserRequest
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
