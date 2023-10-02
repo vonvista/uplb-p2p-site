4c3f379fc7386e61d8e43a9422b791ae9739e9af
iggymiguel <iggymiguel@gmail.com>
2023-10-02 23:06:07 +0800

    Started add review

---
+++ UPLB-P2P.Website/Controllers/ReviewsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UPLB_P2P.Website.Models.ViewModels;
+
+namespace UPLB_P2P.Website.Controllers
+{
+    public class ReviewsController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Add(AddReviewRequest addReviewRequest)
+        {
+            var rating = addReviewRequest.Rating;
+            var title = addReviewRequest.Title;
+            var content = addReviewRequest.Content;
+            return View("Add");
+        }
+
+    }
+}
---
+++ UPLB-P2P.Website/Models/ViewModels/AddReviewRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace UPLB_P2P.Website.Models.ViewModels
+{
+    public class AddReviewRequest
+    {
+        public int Rating { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
